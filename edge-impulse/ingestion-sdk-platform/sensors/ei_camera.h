f1b65055b58b1f88789c108cbf6ab6b125c60b72
Yahia-Abozahra <102634586+Yahia-Abozahra@users.noreply.github.com>
2024-05-21 05:53:43 +0300

    Update ei_camera.h

--- edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
+++ edge-impulse/ingestion-sdk-platform/sensors/ei_camera.h
@@ -39,22 +39,26 @@
   // AI Thinker
   // https://github.com/SeeedDocument/forum_doc/raw/master/reg/ESP32_CAM_V1.6.pdf
   //
-  #define PWDN_GPIO_NUM     32
-  #define RESET_GPIO_NUM    -1
-  #define XCLK_GPIO_NUM      0
-  #define SIOD_GPIO_NUM     26
-  #define SIOC_GPIO_NUM     27
-  #define Y9_GPIO_NUM       35
-  #define Y8_GPIO_NUM       34
-  #define Y7_GPIO_NUM       39
-  #define Y6_GPIO_NUM       36
-  #define Y5_GPIO_NUM       21
-  #define Y4_GPIO_NUM       19
-  #define Y3_GPIO_NUM       18
-  #define Y2_GPIO_NUM        5
-  #define VSYNC_GPIO_NUM    25
-  #define HREF_GPIO_NUM     23
-  #define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM 26
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 32
+#define SIOD_GPIO_NUM 13
+#define SIOC_GPIO_NUM 12
+
+#define Y9_GPIO_NUM 39
+#define Y8_GPIO_NUM 36
+#define Y7_GPIO_NUM 23
+#define Y6_GPIO_NUM 18
+#define Y5_GPIO_NUM 15
+#define Y4_GPIO_NUM 4
+#define Y3_GPIO_NUM 14
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 27
+#define HREF_GPIO_NUM 25
+#define PCLK_GPIO_NUM 19
+
+#define I2C_SDA 21
+#define I2C_SCL 22
   #define LED_PIN           33 // Status led
   #define LED_ON           LOW // - Pin is inverted.
   #define LED_OFF         HIGH //
